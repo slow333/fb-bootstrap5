852281fd98354085f69eb2cf8ee71750306d7130
kim kwan yong <slow33@gmail.com>
2025-06-08 04:21:09 +0900

    bootstrap done

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,13 @@
 async function loadMenu(){
-   await loadHeadLinks();
-   let responseNav = await fetch('/utils/nav.html');
-   let navText = await responseNav.text();
+   await loadHeadCss();
+   let navHtml = await fetch('/utils/nav.html');
+   let navText = await navHtml.text();
    await setNavEl(navText);
+   await loadBootstrapJs();
 }
 loadMenu().catch(error => new Error(error.message));
 
-async function loadHeadLinks(){
+async function loadHeadCss(){
    let links = await fetch('/utils/links.html');
    let linksText = await links.text();
    document.head.insertAdjacentHTML("afterbegin",linksText);
@@ -15,7 +16,14 @@ async function loadHeadLinks(){
 // responseText를 가지고 nav element 생성
 async function setNavEl(navText){
    let nav = document.createElement('nav');
+   // nav.classList.add('fixed-top');
    nav.innerHTML = navText;
    document.body.prepend(nav);
-   return nav
+   // return nav
+}
+// load bootstrap js on startup
+async function loadBootstrapJs() {
+   const script = document.createElement('script');
+   script.src = '/js/bootstrap.bundle.min-5-3.js';
+   document.body.appendChild(script);
 }
